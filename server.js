c95c2c799c2502d7bd757ad413e997592e530b1e
e-a-11 <apollo_estrella@yahoo.com>
2025-06-28 15:16:30 +0800

    trigger CI/CD deploy1.21

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
